b923604b9390d1b1264b728a954a2387484ad9d3
choisungyoung <sungyoungchoi94@gmail.com>
2021-12-18 16:05:47 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/demo/IntApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
