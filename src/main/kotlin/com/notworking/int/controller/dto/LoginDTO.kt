96980ecf0b06cb6645c3768ad9d75ed8281fddd5
choisungyoung <sungyoungchoi94@gmail.com>
2021-12-23 15:06:16 +0900

    refactor : package명 isnt로 수정

--- src/main/kotlin/com/notworking/int/controller/dto/LoginDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.notworking.int.controller.dto
-
-import com.notworking.int.model.Developer
-import com.notworking.int.support.type.Role
-
-data class LoginDTO(
-    var username: String,
-    var password: String
-)
