75ac34152e0f2d80e83f8a0e4c2d55476e42e9a7
choisungyoung <sungyoungchoi94@gmail.com>
2021-12-30 00:08:54 +0900

    chore : add for allowed origins

---
+++ src/main/kotlin/com/notworking/isnt/support/config/WebMvcConfig.kt
@@ -0,0 +1,21 @@
+package com.notworking.isnt.support.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override
+    fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.DELETE.name
+            )
+    }
+}
